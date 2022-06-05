4015097c41456d357d686769e89eb8ad45f1375f
Larissa Moro <54962550+LariMoro20@users.noreply.github.com>
2022-06-05 15:12:02 -0300

    [UPDATE] Change loading and errors messages

--- src/pages/Homepage.vue
+++ src/pages/Homepage.vue
@@ -2,8 +2,32 @@
   <q-page class="flex-center row">
     <div class="q-pa-md text-white text-center">
       <h2>Personagens da série Rick and Morty</h2>
+      <!---->
+      <q-dialog v-model="loading">
+        <q-card class="text-center bg-black text-white">
+          <q-card-section>
+            <div class="text-h6">Carregando personagens...</div>
+            <q-circular-progress
+              indeterminate
+              size="50px"
+              color="lime"
+              class="q-ma-md"
+            />
+          </q-card-section>
+        </q-card>
+      </q-dialog>
+      <q-dialog v-model="error">
+        <q-card class="text-center bg-black text-white">
+          <q-card-section>
+            <div class="text-h6">
+              Erro: Verifique sua conexão com a internet e tente novamente
+            </div>
+          </q-card-section>
+        </q-card>
+      </q-dialog>
       <h6>Pagina {{ current }}</h6>
     </div>
+
     <div class="q-pa-md row" v-if="result && result.characters">
       <div class="q-pa-sm flex flex-center col-12 bg-secondary">
         <q-pagination
@@ -29,12 +53,6 @@
         />
       </div>
     </div>
-    <div v-if="loading" class="col-md-12 q-pa-md">Carregando... aguarde</div>
-    <div v-else-if="error" class="col-md-12 q-pa-md">
-      Erro: Verifique sua conexão com a internet ou VPN e tente novamente<br /><small
-        >[{{ error.message }}]</small
-      >
-    </div>
     <q-page-scroller
       position="bottom-right"
       :scroll-offset="150"
@@ -59,6 +77,7 @@ export default defineComponent({
       page: 1,
     });
     const current = ref(1);
+    const active = ref(true);
     const setPage = async (val) => {
       fetchMore({
         variables: {
@@ -78,6 +97,7 @@ export default defineComponent({
       loading,
       error,
       current,
+      active,
     };
   },
 });
